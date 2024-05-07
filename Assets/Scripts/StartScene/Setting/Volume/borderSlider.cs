33d327fe0c0a884fb07816d8e8d567af32326255
IvanKurikin <ivankurikin12@gmail.com>
2024-05-07 14:09:58 +0300

    main menu volume

--- Assets/Scripts/StartScene/Setting/Volume/borderSlider.cs
+++ Assets/Scripts/StartScene/Setting/Volume/borderSlider.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class borderSlider : MonoBehaviour
@@ -8,29 +10,26 @@ public class borderSlider : MonoBehaviour
     {
         _transform = GetComponent<Transform>();
     }
-
     void FixedUpdate()
     {
-        // Ограничение перемещения по оси X
-        if (_transform.position.x > -6.56f)
+        if (_transform.position.x > -6.6f) 
         {
             _transform.position = new Vector3(-6.56f, 9.35f, -3.70f);
         }
-        if (_transform.position.x < -12.2f)
+        if (_transform.position.x < -12.3f)
         {
             _transform.position = new Vector3(-12.2f, 9.35f, -3.70f);
         }
-
-        // Ограничение перемещения по оси Y
         if (_transform.position.y != 9.3f)
         {
             _transform.position = new Vector3(_transform.position.x, 9.3f, -3.70f);
         }
+        if (_transform.position.x < -6.7f && _transform.position.x > -12.3f)
+        {
+            PlayerPrefs.SetFloat("totalVolume", ((_transform.position.x - (-6.6f)) / -0.056f)/100);
+            Debug.Log((_transform.position.x - (-6.6f)) / -0.056f);
+        }
 
-        // Вычисление и сохранение значения громкости
-        float normalizedVolume = Mathf.InverseLerp(-6.7f, -12.3f, _transform.position.x);
-        float scaledVolume = Mathf.Lerp(0f, 1f, normalizedVolume);
-        PlayerPrefs.SetFloat("totalVolume", scaledVolume);
-        PlayerPrefs.Save();
+
     }
 }
--- Assets/Scripts/StartScene/Setting/Volume/volumeSlider.cs
+++ Assets/Scripts/StartScene/Setting/Volume/volumeSlider.cs
@@ -2,20 +2,10 @@ using UnityEngine;
 
 public class volumeSlider : MonoBehaviour
 {
+    public GameObject _checkBox;
+    public Vector3 GameObj;
     public GameObject objectToMove;
-    public AudioSource audioSource;
-
     private void OnMouseDown()
-    {
-        UpdateVolume();
-    }
-
-    private void OnMouseDrag()
-    {
-        UpdateVolume();
-    }
-
-    private void UpdateVolume()
     {
         Vector3 mouse = Input.mousePosition;
         Ray castPoint = Camera.main.ScreenPointToRay(mouse);
@@ -23,19 +13,21 @@ public class volumeSlider : MonoBehaviour
         if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
         {
             objectToMove.transform.position = hit.point;
-
-            // Получение нормализованного значения громкости на основе позиции объекта
-            float normalizedVolume = Mathf.InverseLerp(-6.7f, -12.3f, objectToMove.transform.position.x);
-
-            // Масштабирование нормализованного значения громкости в интервал от 0 до 1
-            float scaledVolume = Mathf.Lerp(0f, 1f, normalizedVolume);
-
-            // Применение нового значения громкости к AudioSource
-            audioSource.volume = scaledVolume;
-
-            // Сохранение значения громкости
-            PlayerPrefs.SetFloat("totalVolume", scaledVolume);
-            PlayerPrefs.Save();
         }
+        
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 mouse = Input.mousePosition;
+        Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+        RaycastHit hit;
+        if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
+        {
+            
+            objectToMove.transform.position = hit.point;
+            
+        }
+        
     }
 }
